de548231153d52928c7a74c9e46f300229063328
user <ciyeda@master-mail.net>
2019-06-19 22:07:17 -0300

    Protegendo a função

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -1,38 +1,33 @@
-(function () {
-const url = 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs';
+const posts = (function () {
+    const url = 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs';
 
-fetch(url)
-    .then(function(response){
-        if (response.ok){
-            response.json()
-                .then(
-                    function(data){
-                        var blogWrapper = document.getElementById("demo");
-                        var allPosts = data.map(item => {
-                            var capitalLetter = item.title.charAt(0).toUpperCase(0);
+    return {
+        get_posts: function(){
+            fetch(url)
+            .then(function(response){
+                if (response.ok){
+                    response.json()
+                    .then(
+                        function(data){
+                            var blogWrapper = document.getElementById("demo");
+                            var allPosts = data.map(item => {
+                                var capitalLetter = item.title.charAt(0).toUpperCase(0);
+                                
+                                var title = `<h2 ='blog-post-title'>${capitalLetter + item.title.slice(1)}</h2>`;
+                                var meta = `<p class='blog-post-meta'>Post #${item.id}</p>`;
+                                var body = `<p>${item.body}</p>`;
+                                
+                                var blogPost = `<div class='blog-post'>${title + meta + body + '<hr/>' + body}</div>`;
+    
+                                return blogPost;
+                            })
+                            .splice(0,4)
+                            .join("")
                             
-                            var title = `<h2 ='blog-post-title'>${capitalLetter + item.title.slice(1)}</h2>`;
-                            var meta = `<p class='blog-post-meta'>Post #${item.id}</p>`;
-                            var body = `<p>${item.body}</p>`;
-                            
-                            var blogPost = `<div class='blog-post'>${title + meta + body + '<hr/>' + body}</div>`;
-
-                            return blogPost;
-                        })
-                        .splice(0,4)
-                        .join("")
-                        
-                        blogWrapper.innerHTML = allPosts;
-                    }
-                )
+                            blogWrapper.innerHTML = allPosts;
+                    })
+                }
+            })
         }
-})
-
-//function reqListener() {
-//    console.log(this.responseText);
-//}
-//var oReq = new XMLHttpRequest();
-//oReq.onload = reqListener;
-//oReq.open('GET', 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs', true);
-//oReq.send();
+    }
 })()
