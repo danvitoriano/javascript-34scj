c9d1eada22c8c68adc6dcb5ff623a327c7b07e03
rodrigoalmeidaas <rodrigo.almeida@agronow.com.br>
2019-06-19 22:06:59 -0300

    Added module pattern

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -9,13 +9,33 @@
 // oReq.open('get', 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs');
 // oReq.send();
 
-function appendElement(dom, data) {
-  dom.insertAdjacentHTML('beforeend', `<h4>${data.title}</h4>`);
-  dom.insertAdjacentHTML('beforeend', `<p>${data.body}</p>`);
-}
+const posts = (function() {
+  function appendElement(dom, data) {
+    let capitalized = data.title.charAt(0).toUpperCase();
 
-const url = 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs';
-fetch(url)
-  .then(response => response.ok ? (response.json()
-  .then(data => data.forEach(element => appendElement(document.getElementById("demo"), element))))
-  : console.log('Error'));
+    const blogPost = `
+      <div class='blog-post'>
+        <h2 class='blog-post-title'>${capitalized + data.title.slice(1)}</h2>
+        <p class='blog-post-meta'><a href='#'>Post #${data.id}</a></p>
+        <p>${data.body}</p>
+        <p>${data.body}</p>
+        <p>${data.body}</p>
+        <p>${data.body}</p>
+        <p>${data.body}</p>
+      <div>`;
+    
+    dom.insertAdjacentHTML('beforeend', blogPost);
+  }
+  
+  function fetchPosts() {
+    const url = 'https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs';
+    fetch(url)
+      .then(response => response.ok ? (response.json()
+      .then(data => data.splice(0, 4).forEach(element => appendElement(document.getElementById("demo"), element))))
+      : console.log('Error'));
+  }
+
+  return {
+    getPosts: fetchPosts
+  }
+}().getPosts())
