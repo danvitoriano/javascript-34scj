605fb865cb106ea6e40c0da9a9ed0297d8729bc3
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-06-19 22:02:28 -0300

    IIFE - "encapsulamento"

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -1,19 +1,21 @@
-const url = "https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs";
+(function () {
+    
+    const url = "https://5d04064fd1471e00149bb174.mockapi.io/api/v1/blogs";
 
-fetch(url)
-    .then(response => {
-        // console.log("respose=>",response);
-        if(response.ok){
-            response
-                .json()
-                .then(data => { 
-                    // console.log("data=> ",data);
-                    var blogWrapper = document.getElementById("blog-posts");
-                    var allPosts = data.map((post,index)=>{
-                        //console.log("post=>",post);
-                        var postCreatedAt = new Date(post.createdAt).toDateString();
-                        var capitalLetter = post.title.charAt(0).toUpperCase();
-                        return `
+    fetch(url)
+        .then(response => {
+            // console.log("respose=>",response);
+            if (response.ok) {
+                response
+                    .json()
+                    .then(data => {
+                        // console.log("data=> ",data);
+                        var blogWrapper = document.getElementById("blog-posts");
+                        var allPosts = data.map((post, index) => {
+                            //console.log("post=>",post);
+                            var postCreatedAt = new Date(post.createdAt).toDateString();
+                            var capitalLetter = post.title.charAt(0).toUpperCase();
+                            return `
                             <div class="blog-post">
                                 <h2 class="blog-post-title">${capitalLetter + post.title.slice(1)}</h2>
                                 <p class="blog-post-meta">Post#<a href="#">${post.id}</a> - ${postCreatedAt}</p>
@@ -21,23 +23,25 @@ fetch(url)
                                 <p>${post.body} ${post.body} ${post.body} ${post.body} ${post.body} ${post.body}</p>
                             </div>                       
                         `;
-                    })
-                    .splice(0,4)
-                    .join("");
-                    blogWrapper.innerHTML = allPosts;
-                });
-        }
-    });
+                        })
+                            .splice(0, 4)
+                            .join("");
+                        blogWrapper.innerHTML = allPosts;
+                    });
+            }
+        });
 
 
 
 
-// function reqListener() {
-//     console.log("reqListener  -> responseText", this.responseText);
-//     alert(this.responseText);
-// }
+    // function reqListener() {
+    //     console.log("reqListener  -> responseText", this.responseText);
+    //     alert(this.responseText);
+    // }
 
-// var xmlHttpRequest = new XMLHttpRequest();
-// xmlHttpRequest.onload = reqListener;
-// xmlHttpRequest.open("GET", url, true);
-// xmlHttpRequest.send();
+    // var xmlHttpRequest = new XMLHttpRequest();
+    // xmlHttpRequest.onload = reqListener;
+    // xmlHttpRequest.open("GET", url, true);
+    // xmlHttpRequest.send();
+
+}());
